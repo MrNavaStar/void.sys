28ca2a8ec1e64fbb3d109c2c862e53f99508de9c
MrNavaStar <mr.navastar@gmail.com>
2025-05-23 21:39:19 -0600

    camera feels kinda chill

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,12 +1,9 @@
-extends Camera3D
+extends Node3D
 
-@export var speed = 0.5
-@export var height = 2
+@export var speed = 0.2
 var target: Vector3
 
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
-	
-	target += direction * speed
+	target += (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized() * speed
 	position = lerp(position, target, 0.05)
