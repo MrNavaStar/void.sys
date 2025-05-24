0fd202946edeaca2e2b93f903725378b1251e3f4
MrNavaStar <mr.navastar@gmail.com>
2025-05-23 21:10:31 -0600

    start world impl

---
+++ world.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+@export var planets = 100
+@export var ships = 100
+@export var radius = 1000
+
+func spawn_nodes(planet: Resource) -> void:
+	for i in planets:
+		var x = randi() % radius
+		var z = randi() % radius
+		var instance = planet.instantiate()
+		instance.set_position(Vector3(x, 0, z))
+		print(Vector3(x, 0, z))
+		add_child(instance)
+	
+func _on_ready() -> void:
+	seed(12345)
+	var planet = preload("res://planet.tscn")
+	
+	spawn_nodes(planet)
