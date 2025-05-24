9e57950f2aa2457c798774d8125309b47165deeb
MrNavaStar <mr.navastar@gmail.com>
2025-05-23 22:02:44 -0600

    camera zoom!

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,9 +1,21 @@
 extends Node3D
 
 @export var speed = 0.2
+@export var zoom_speed = 0.2
+
+@onready var camera = $Camera3D
 var target: Vector3
+var zoom_target: float
+
+func _on_ready() -> void:
+	target = position
+	zoom_target = position.z
 
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	target += (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized() * speed
 	position = lerp(position, target, 0.05)
+	
+	var zoom_direction = int(Input.is_action_just_released("zoom_out")) - int(Input.is_action_just_released("zoom_in"))
+	zoom_target += zoom_direction * speed
+	camera.position.z = lerp(camera.position.z, zoom_target, 0.1)
