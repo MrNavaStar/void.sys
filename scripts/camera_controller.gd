ec681e5d7659ba2f9d6dca05dcc70aa27893f185
MrNavaStar <mr.navastar@gmail.com>
2025-05-23 22:02:55 -0600

    actually add files

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -3,13 +3,13 @@ extends Node3D
 @export var speed = 0.2
 @export var zoom_speed = 0.2
 
-@onready var camera = $Camera3D
+@onready var camera = $ZoomPivot/Camera3D
 var target: Vector3
 var zoom_target: float
 
 func _on_ready() -> void:
 	target = position
-	zoom_target = position.z
+	zoom_target = camera.position.z
 
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("left", "right", "up", "down")
